b01c488f518c396968952166a558206ca447ca7e
leehyoenjong <187351492+susuProgrammer@users.noreply.github.com>
2025-04-30 07:04:56 +0900

    곱셈 2588

---
+++ C#/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int value1 = Convert.ToInt32(Console.ReadLine());
+        int value2 = Convert.ToInt32(Console.ReadLine());
+
+        string strvalue2 = value2.ToString();
+
+        for (int i = strvalue2.Length - 1; i >= 0; i--)
+        {
+            int digit = Convert.ToInt32(strvalue2[i].ToString());
+            Console.WriteLine(Convert.ToInt32(value1 * digit));
+        }
+        int result = value1 * value2;
+        Console.WriteLine(Convert.ToInt32(result));
+    }
+}
---
+++ C#/num_11382.cs
@@ -0,0 +1,9 @@
+public class num_11382
+{
+    public static void Main()
+    {
+        int value1 = Convert.ToInt32(Console.ReadLine());
+        int value2 = Convert.ToInt32(Console.ReadLine());
+        int value3 = Convert.ToInt32(Console.ReadLine());
+    }
+}
