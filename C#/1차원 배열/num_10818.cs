a9326e6eff09062f8d28013289d94af6056083ed
leehyoenjong <187351492+susuProgrammer@users.noreply.github.com>
2025-05-01 17:13:26 +0900

    일차원 배열열

---
+++ C#/1차원 배열/num_10818.cs
@@ -0,0 +1,36 @@
+public class num_10818
+{
+    public static void Main()
+    {
+        Arr();
+        List();
+    }
+    static void Arr()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        string[] input = Console.ReadLine().Split();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            var value = int.Parse(input[i]);
+            arr[i] = value;
+        }
+        Console.WriteLine($"{arr.Min()} {arr.Max()}");
+    }
+    static void List()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        string[] input = Console.ReadLine().Split();
+        List<int> list = new List<int>();
+        for (int i = 0; i < N; i++)
+        {
+            var value = int.Parse(input[i]);
+            list.Add(value);
+        }
+        Console.WriteLine($"{list.Min()} {list.Max()}");
+    }
+
+
+}
